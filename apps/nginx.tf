04605731f23a156b2dfb925e6750218fa9005554
Mi-nos <gm49261@zut.edu.pl>
2025-09-17 13:00:44 +0200

    First homelab commit

---
+++ apps/nginx.tf
@@ -0,0 +1,57 @@
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx"
+    namespace = kubernetes_namespace.demo.metadata[0].name
+    labels = {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+
+      spec {
+        container {
+          name  = "nginx"
+          image = "nginx:1.25"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name      = "nginx-service"
+    namespace = kubernetes_namespace.demo.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = kubernetes_deployment.nginx.metadata[0].labels.app
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+      node_port   = 30080
+    }
+
+    type = "NodePort"
+  }
+}
